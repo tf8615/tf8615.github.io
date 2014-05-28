f106cf6b84f59301f03353c136f2d93e44b0830a
tf8615 <tf8615@gmail.com>
2014-05-28 13:54:09 +0800

    Site updated at 2014-05-28 05:54:09 UTC

---
+++ git-guide/js/projects.js
@@ -0,0 +1,11 @@
+$(function(){
+
+var sitenavbar = "<div id=\"site-navbar\" style=\"position: absolute; top: -4px; left: -3px; border: 0; z-index: 2000;padding:0;margin:0;\">" +
+			"<a href=\"/\" title=\"返回首页\" style=\"background:none;\">" +
+				"<img src=\"img/return-back.png\" style=\"padding:0;margin:0;border:0; -webkit-box-shadow: none;-moz-box-shadow: none;box-shadow: none;\">"+
+			"</a>"+
+		"</div>";
+
+	$("body").append(sitenavbar);
+
+});
